447775094c3a5e54730b3ba9086617c61d2d9043
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-05-22 04:22:30 -0400

    Plantilla comun del website

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Head from "next/head";
+import Container from "@material-ui/core/Container";
+
+import Navbar from "./Navbar";
+
+export default function Layout({ children, title }) {
+  return (
+    <>
+      <Head>
+        <title>{title || "Home"}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <main>
+        {/* <Toolbar /> */}
+        <Container>{children}</Container>
+      </main>
+    </>
+  );
+}
