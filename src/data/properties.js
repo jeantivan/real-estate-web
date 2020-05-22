ad8f49eda6c838bf32d0057a7aed83e80b5e46df
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-05-22 04:19:26 -0400

    Data de ejemplo

---
+++ src/data/properties.js
@@ -0,0 +1,10 @@
+export const properties = [
+  {
+    title: "",
+    price: "",
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci ipsum ducimus distinctio 
+    necessitatibus obcaecati doloribus nulla sunt cum fugit ad culpa officia sequi illum amet, recusandae totam!
+    Quas, consectetur nihil.`,
+    images: [],
+  },
+];
