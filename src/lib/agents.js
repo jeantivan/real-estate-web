27e30daa4ac1867027761e32e1b32cd0dba4b1c9
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-05-24 19:16:56 -0400

    Obtener data de los agentes y los inmuebles

---
+++ src/lib/agents.js
@@ -0,0 +1,9 @@
+import { agents } from "../data/agents";
+
+export function getAgents() {
+  return agents;
+}
+
+export function getAgentById(id) {
+  return agents.find((agent) => agent.id == id);
+}
---
+++ src/lib/inmuebles.js
@@ -0,0 +1,69 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import remark from "remark";
+import html from "remark-html";
+
+import { getAgentById } from "./agents";
+
+const inmueblesDirectory = path.join(process.cwd(), "src/data/inmuebles");
+
+export function getInmueblesList() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(inmueblesDirectory);
+  const inmuebleList = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const slug = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(inmueblesDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      slug,
+      ...matterResult.data,
+    };
+  });
+
+  return inmuebleList;
+}
+
+export function getAllInmueblesSlugs() {
+  const fileNames = fs.readdirSync(inmueblesDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        slug: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getInmuebleData(slug) {
+  const fullPath = path.join(inmueblesDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  const agent = getUserById(matterResult.agentId);
+  // Combine the data with the id and contentHtml
+  return {
+    slug,
+    contentHtml,
+    ...matterResult.data,
+    agent,
+  };
+}
