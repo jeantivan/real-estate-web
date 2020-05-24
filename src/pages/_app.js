fbb337150a144f110df5ed56512d6a0c5105f2df
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-05-24 02:00:26 -0400

    Configurados los estilos de fontawesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,9 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
