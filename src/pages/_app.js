f9b9bfc211cc6374c17f666eeed76b8da1080645
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-10 17:54:44 -0400

    Movidos el footer y el navbar a la pagina _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types";
 import Head from "next/head";
+
+// Material-UI config
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../theme";
 
+// FontAwesome config
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
@@ -21,16 +27,20 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>Avilatek</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Navbar />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </React.Fragment>
   );
