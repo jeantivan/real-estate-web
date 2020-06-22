fcdfc7112660f388b2ca571ce925599b9cc5a5c8
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-22 17:05:08 -0400

    Agregado el formulario de contacto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,24 +1,26 @@
 import Layout from "../components/Layout";
-import Typography from "@material-ui/core/Typography";
+import ContactForm from "../components/ContactForm";
+
+import { Typography, Grid } from "@material-ui/core";
 
 export default function Contacto() {
   return (
     <Layout titulo="Contacto">
-      <Typography variant="h1" component="h1">
+      <Typography variant="h2" component="h1">
         Contacto
       </Typography>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-        repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-        perferendis temporibus id ex, neque rem voluptate quis tempora esse
-        earum alias voluptates.
-      </Typography>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-        repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-        perferendis temporibus id ex, neque rem voluptate quis tempora esse
-        earum alias voluptates.
-      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography>
+            Obcaecati repellendus blanditiis tenetur facilis nam architecto iure
+            aspernatur perferendis temporibus id ex, neque rem voluptate quis
+            tempora esse earum alias voluptates.
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <ContactForm />
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
