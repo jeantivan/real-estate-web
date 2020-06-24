54d182ddb2f91dcb4af0dca1958282f178ef8c8c
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-24 14:26:00 -0400

    Agregado NProgress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import Head from "next/head";
+import Router from "next/router";
 
 // Material-UI config
 import { ThemeProvider } from "@material-ui/core/styles";
@@ -10,6 +11,12 @@ import theme from "../theme";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
+// Nprogress config
+import NProgress from "nprogress";
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -42,6 +49,93 @@ export default function MyApp(props) {
         <Component {...pageProps} />
         <Footer />
       </ThemeProvider>
+
+      {/* Nprogress Styles */}
+      <style jsx global>
+        {`
+          /* Make clicks pass-through */
+          #nprogress {
+            pointer-events: none;
+          }
+
+          #nprogress .bar {
+            background: #2196f3;
+
+            position: fixed;
+            z-index: 9999;
+            top: 0;
+            left: 0;
+
+            width: 100%;
+            height: 2px;
+          }
+
+          /* Fancy blur effect */
+          #nprogress .peg {
+            display: block;
+            position: absolute;
+            right: 0px;
+            width: 100px;
+            height: 100%;
+            box-shadow: 0 0 10px #2196f3, 0 0 5px #2196f3;
+            opacity: 1;
+
+            -webkit-transform: rotate(3deg) translate(0px, -4px);
+            -ms-transform: rotate(3deg) translate(0px, -4px);
+            transform: rotate(3deg) translate(0px, -4px);
+          }
+
+          /* Remove these to get rid of the spinner */
+          #nprogress .spinner {
+            display: block;
+            position: fixed;
+            z-index: 1031;
+            top: 15px;
+            right: 15px;
+          }
+
+          #nprogress .spinner-icon {
+            width: 18px;
+            height: 18px;
+            box-sizing: border-box;
+
+            border: solid 2px transparent;
+            border-top-color: #2196f3;
+            border-left-color: #2196f3;
+            border-radius: 50%;
+
+            -webkit-animation: nprogress-spinner 400ms linear infinite;
+            animation: nprogress-spinner 400ms linear infinite;
+          }
+
+          .nprogress-custom-parent {
+            overflow: hidden;
+            position: relative;
+          }
+
+          .nprogress-custom-parent #nprogress .spinner,
+          .nprogress-custom-parent #nprogress .bar {
+            position: absolute;
+          }
+
+          @-webkit-keyframes nprogress-spinner {
+            0% {
+              -webkit-transform: rotate(0deg);
+            }
+            100% {
+              -webkit-transform: rotate(360deg);
+            }
+          }
+          @keyframes nprogress-spinner {
+            0% {
+              transform: rotate(0deg);
+            }
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+        `}
+      </style>
     </React.Fragment>
   );
 }
