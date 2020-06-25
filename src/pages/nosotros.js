8a11744ca8cfd61631ce56252be5c12f834b6151
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-25 19:50:18 -0400

    Añadido contenido nuevo

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,7 +1,26 @@
 import Layout from "../components/Layout";
-import Typography from "@material-ui/core/Typography";
+import Agent from "../components/Agent";
+import { Typography, Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Nosotros() {
+import { getAllAgents } from "../lib/agents";
+
+const useStyles = makeStyles((theme) => ({
+  equipo: {
+    margin: theme.spacing(2, 0),
+  },
+  agent: {
+    width: "100%",
+    minHeight: 300,
+  },
+}));
+
+export default function Nosotros({ results: agents }) {
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    console.log(agents);
+  }, []);
   return (
     <Layout
       titulo="Nosotros"
@@ -11,17 +30,70 @@ export default function Nosotros() {
         Nosotros
       </Typography>
       <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-        repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-        perferendis temporibus id ex, neque rem voluptate quis tempora esse
-        earum alias voluptates.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis risus
+        odio, ultricies sit amet velit a, hendrerit mattis turpis. Sed sed magna
+        eget felis auctor luctus. Donec eget turpis ligula. Cras pharetra
+        consequat mauris, in vehicula turpis gravida eget. Fusce ut ex rhoncus,
+        fermentum mauris in, condimentum tortor. Suspendisse at turpis felis.
+        Nulla non neque efficitur, sodales lectus in, pharetra magna. Curabitur
+        ut dapibus libero. In mattis, ligula in condimentum efficitur, purus
+        ipsum efficitur leo, a semper justo lectus id tellus. Quisque sodales
+        vehicula nisi, vitae porta ipsum fringilla sodales.
       </Typography>
       <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-        repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-        perferendis temporibus id ex, neque rem voluptate quis tempora esse
-        earum alias voluptates.
+        Phasellus feugiat ut orci sed imperdiet. Curabitur consectetur efficitur
+        lacus id posuere. In sapien massa, luctus et rhoncus non, pretium sit
+        amet eros. Donec molestie dictum odio, at lacinia mauris auctor et.
+        Curabitur iaculis, elit at ultrices finibus, turpis felis pellentesque
+        eros, in volutpat nibh nibh vitae augue. Nam ac dolor fermentum quam
+        pellentesque commodo ut sit amet augue. Quisque nisi lectus, vehicula
+        sed ex in, molestie mollis elit. Integer ut auctor ipsum, at fringilla
+        erat. In sapien libero, pharetra in accumsan ut, sollicitudin in libero.
+        Donec non commodo sapien. Donec nec sollicitudin velit, a convallis
+        tortor. Integer diam lectus, dapibus vitae molestie eu, venenatis vel
+        ipsum. Nulla cursus purus quis mi varius semper. Mauris ante enim,
+        ultrices vehicula elit sit amet, pulvinar convallis tellus.
       </Typography>
+      <Typography>
+        Donec a consectetur purus. Nullam vel neque quis tortor malesuada rutrum
+        ac in tortor. Phasellus cursus magna eu diam elementum venenatis.
+        Curabitur vehicula placerat sapien, a consectetur neque viverra in.
+        Donec sollicitudin ligula ac neque elementum, vitae dapibus lectus
+        molestie. Donec risus massa, hendrerit in ligula eget, feugiat
+        condimentum mauris. Donec at diam non felis pulvinar semper.
+        Pellentesque suscipit elementum dolor, quis euismod ex lacinia id.
+        Praesent lorem tortor, tempus ac lorem non, convallis finibus tellus.
+        Fusce quis odio convallis tortor scelerisque volutpat vitae vel nisl. In
+        quis turpis leo.
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        component="section"
+        id="equipo"
+        className={classes.equipo}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h2">
+            Nuestros Agentes
+          </Typography>
+        </Grid>
+        {agents.map(({ data, id }) => (
+          <Grid item xs={12} md={6} lg={3} key={id}>
+            <Agent {...{ ...data, id }} />
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const { results } = await getAllAgents();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
