9d1c418302904a8e980a993bd8d559c98e6074fa
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-24 20:44:18 -0400

    No se exportan variables importantes

--- src/prismic-config.js
+++ src/prismic-config.js
@@ -1,11 +1,11 @@
 import Prismic from "prismic-javascript";
 
-export const API_TOKEN = process.env.PRISMIC_API_TOKEN;
-export const API_ENPOINT = process.env.PRISMIC_API_ENDPOINT;
-
 // Client method to query documents from the Prismic repo
 export const PrismicClient = (req = null) =>
-  Prismic.client(API_ENPOINT, createClientOptions(req, API_TOKEN));
+  Prismic.client(
+    process.env.PRISMIC_API_ENDPOINT,
+    createClientOptions(req, process.env.PRISMIC_API_TOKEN)
+  );
 
 const createClientOptions = (req = null, prismicAccessToken = null) => {
   const reqOption = req ? { req } : {};
