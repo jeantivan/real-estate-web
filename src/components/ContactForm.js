1a32b30f3c60da95045e1e944b7c9d02155a695c
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-25 22:05:14 -0400

    Formulario listo

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,31 @@
-import { Button, TextField, Typography, Paper } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Typography,
+  Paper,
+  Snackbar,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
+
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  nombre: "",
+  email: "",
+  mensaje: "",
+};
+
+const validationSchema = Yup.object({
+  nombre: Yup.string().required(
+    "Campo obligatorio. Por favor escribe tu nombre"
+  ),
+  email: Yup.string().email("Email invalido.").required("Campo obligatorio"),
+  mensaje: Yup.string()
+    .max(255, "Mensaje muy largo, maximo 255 caracteres")
+    .required("Campo obligatorio"),
+});
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -10,48 +36,97 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const MyInput = (props) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <TextField
+      error={meta.touched && meta.error}
+      helperText={meta.touched ? meta.error : null}
+      {...props}
+      {...field}
+    />
+  );
+};
+
 const ContactForm = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
   return (
-    <Paper className={classes.container}>
+    <Paper className={classes.container} elevation={2}>
       <Typography variant="h6" component="h2">
         Llena el siguiente formulario.
       </Typography>
-      <TextField
-        autoFocus
-        id="nombre"
-        label="Nombre"
-        type="text"
-        margin="normal"
-        variant="outlined"
-        size="small"
-        fullWidth
-      />
-      <TextField
-        id="email"
-        label="Correo electronico"
-        type="email"
-        margin="normal"
-        variant="outlined"
-        size="small"
-        fullWidth
-      />
-      <TextField
-        id="mensaje"
-        label="Mensaje"
-        type="text"
-        multiline
-        rows={6}
-        maxRows={6}
-        margin="normal"
-        variant="outlined"
-        fullWidth
-      />
-      <div className={classes.submit}>
-        <Button color="primary" variant="contained" disableElevation fullWidth>
-          Enviar
-        </Button>
-      </div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(_, { setSubmitting, resetForm }) => {
+          resetForm();
+          setTimeout(() => {
+            setOpen(true);
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        {({ submitForm, isSubmitting }) => (
+          <Form>
+            <MyInput
+              autoFocus
+              name="nombre"
+              label="Nombre"
+              type="text"
+              margin="normal"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+            <MyInput
+              name="email"
+              label="Correo electronico"
+              type="email"
+              margin="normal"
+              variant="outlined"
+              size="small"
+              fullWidth
+            />
+            <MyInput
+              name="mensaje"
+              label="Mensaje"
+              multiline
+              rows={6}
+              rowsMax={6}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+            <div className={classes.submit}>
+              <Button
+                disabled={isSubmitting}
+                onClick={submitForm}
+                color="primary"
+                variant="contained"
+                disableElevation
+                fullWidth
+              >
+                Enviar
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <Alert elevation={6} variant="filled" severity="success">
+          Gracias por tu contactarnos
+        </Alert>
+      </Snackbar>
     </Paper>
   );
 };
