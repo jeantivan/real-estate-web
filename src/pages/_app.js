323696d9e74beb8fcd1595cca488a30c106ba58f
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-25 20:17:34 -0400

    Solo mostrar la barra cuando la ruta cambia

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,7 @@ config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatic
 
 // Nprogress config
 import NProgress from "nprogress";
+NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
