81440da4d456c414f5ef330fcc51da7055767ee4
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-06-25 22:28:32 -0400

    Custom TextField para uso con formik

---
+++ src/components/TextField.js
@@ -0,0 +1,17 @@
+import TextField from "@material-ui/core/TextField";
+import { useField } from "formik";
+
+const MyInput = (props) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <TextField
+      error={meta.touched && meta.error}
+      helperText={meta.touched ? meta.error : null}
+      {...props}
+      {...field}
+    />
+  );
+};
+
+export default MyInput;
