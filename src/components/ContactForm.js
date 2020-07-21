1c7144de54d5546a2d02d3d5a60bc77f1c895ac4
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-07-20 22:02:16 -0400

    Diseño de la pagina de contacto

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import { Button, Typography, Paper } from "@material-ui/core";
+import { Button, Typography, Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { useSnackbar } from "notistack";
 
@@ -14,19 +14,37 @@ const initialValues = {
 };
 
 const validationSchema = object({
-  nombre: string().required("Campo obligatorio. Por favor escribe tu nombre"),
+  nombre: string().max(255, "Máximo 255 caracteres"),
   email: string().email("Email invalido.").required("Campo obligatorio"),
   mensaje: string()
-    .max(255, "Mensaje muy largo, maximo 255 caracteres")
+    .max(255, "Máximo 255 caracteres")
     .required("Campo obligatorio"),
 });
 
 const useStyles = makeStyles((theme) => ({
+  title: {
+    ...theme.typography.h6,
+    fontWeight: 500,
+    color: theme.palette.grey[500],
+  },
   container: {
     padding: theme.spacing(2, 3),
   },
-  submit: {
-    marginTop: theme.spacing(2),
+  textField: {
+    color: "#fff",
+    "& .MuiFormLabel-root": {
+      color: "rgba(255, 255, 255, 0.7);",
+    },
+    "& .MuiFilledInput-root": {
+      color: "#fff",
+      backgroundColor: "rgba(255, 255, 255, 0.09)",
+      "&:hover": {
+        backgroundColor: "rgba(255, 255, 255, 0.13)",
+      },
+    },
+    "& .MuiFilledInput-underline:before": {
+      borderBottom: "1px solid rgba(255, 255, 255, 0.7);",
+    },
   },
 }));
 
@@ -35,76 +53,86 @@ const ContactForm = () => {
   const { enqueueSnackbar } = useSnackbar();
 
   return (
-    <Paper className={classes.container} elevation={2}>
-      <Typography variant="h6" component="h2">
-        Envianos un correo electrónico.
-      </Typography>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(_, { setSubmitting, resetForm }) => {
-          resetForm();
-          setTimeout(() => {
-            enqueueSnackbar(
-              "Mensaje enviado con éxito. Gracias por contactarnos.",
-              {
-                variant: "success",
-                anchorOrigin: {
-                  vertical: "top",
-                  horizontal: "center",
-                },
-              }
-            );
-            setSubmitting(false);
-          }, 500);
-        }}
-      >
-        {({ submitForm, isSubmitting }) => (
-          <Form onSubmit={submitForm}>
-            <TextField
-              name="nombre"
-              label="Nombre"
-              type="text"
-              margin="normal"
-              variant="outlined"
-              size="small"
-              fullWidth
-            />
-            <TextField
-              name="email"
-              label="Correo electronico"
-              type="email"
-              margin="normal"
-              variant="outlined"
-              size="small"
-              fullWidth
-            />
-            <TextField
-              name="mensaje"
-              label="Mensaje"
-              multiline
-              rows={6}
-              rowsMax={6}
-              margin="normal"
-              variant="outlined"
-              fullWidth
-            />
-            <div className={classes.submit}>
-              <Button
-                disabled={isSubmitting}
-                onClick={submitForm}
-                color="primary"
-                variant="contained"
-                disableElevation
-                fullWidth
-              >
-                Enviar
-              </Button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </Paper>
+    <Grid container spacing={2} direction="column">
+      <Grid item xs={12}>
+        <Typography component="div" className={classes.theme}>
+          Dejanos un mensaje.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(_, { setSubmitting, resetForm }) => {
+            resetForm();
+            setTimeout(() => {
+              enqueueSnackbar(
+                "Mensaje enviado con éxito. Gracias por contactarnos.",
+                {
+                  variant: "success",
+                  anchorOrigin: {
+                    vertical: "top",
+                    horizontal: "center",
+                  },
+                }
+              );
+              setSubmitting(false);
+            }, 500);
+          }}
+        >
+          {({ submitForm, isSubmitting }) => (
+            <Form onSubmit={submitForm}>
+              <Grid container spacing={1}>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    name="nombre"
+                    label="Nombre"
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    className={classes.textField}
+                  />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <TextField
+                    name="email"
+                    label="Correo electronico"
+                    type="email"
+                    variant="filled"
+                    fullWidth
+                    className={classes.textField}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="mensaje"
+                    label="Mensaje"
+                    multiline
+                    rows={6}
+                    rowsMax={10}
+                    variant="filled"
+                    fullWidth
+                    className={classes.textField}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    disabled={isSubmitting}
+                    onClick={submitForm}
+                    color="primary"
+                    variant="contained"
+                    disableElevation
+                    fullWidth
+                  >
+                    Enviar
+                  </Button>
+                </Grid>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,13 +1,45 @@
 import { Layout, ContactForm } from "../components";
 
-import { Typography, Grid, Container, Paper, Box } from "@material-ui/core";
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faWhatsapp,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 const useStyles = makeStyles((theme) => ({
+  bgLight: {
+    backgroundColor: "#fafafa",
+  },
+  bgDark: {
+    backgroundColor: "#000",
+    color: "white",
+  },
+  title: {
+    ...theme.typography.h6,
+    fontWeight: 500,
+    textTransform: "uppercase",
+    letterSpacing: 2,
+  },
+  phoneNumber: {
+    ...theme.typography.h3,
+    fontWeight: "bold",
+    margin: theme.spacing(1, 0),
+  },
   map: {
     width: "100%",
     height: 0,
-    paddingTop: "42.86%",
+    paddingTop: "calc(9 / 21 * 100%)",
     position: "relative",
   },
   mapWrapper: {
@@ -16,45 +48,190 @@ const useStyles = makeStyles((theme) => ({
     left: 0,
     bottom: 0,
     right: 0,
-    padding: theme.spacing(1),
-    borderRadius: "inherit",
+    width: "100%",
+    height: "100%",
+    backgroundColor: theme.palette.grey[500],
   },
   mapContent: {
     width: "100%",
     height: "100%",
-    backgroundColor: theme.palette.grey[500],
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "white",
+  },
+  rsTitle: {
+    ...theme.typography.h5,
+    fontWeight: "bold",
+    textAlign: "center",
+    margin: theme.spacing(2, 0),
+    marginTop: 0,
+  },
+  icon: {
+    color: theme.palette.text.secondary,
+    transition: theme.transitions.create(["color", "transform"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+
+    "&:hover": {
+      transform: "scale(1.1)",
+      color: theme.palette.primary.main,
+    },
+  },
+  info: {
+    backgroundColor: "white",
+    color: "#000",
+    height: "100%",
+    padding: theme.spacing(3),
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
   },
 }));
 
 export default function Contacto() {
   const classes = useStyles();
+
   return (
     <Layout
       titulo="Contacto"
       descripcion="Praesent euismod massa quis iaculis feugiat. Aliquam in pellentesque purus. Cras sit amet risus ut dui accumsan blandit non pellentesque augue. Mauris nisi quam, venenatis nec rutrum sit amet, accumsan sed dolor. Fusce lobortis, erat quis mollis vulputate, lacus tellus venenatis sem, non tempor nisi quam et nisl. Vivamus sollicitudin, nisi a mollis consequat, sapien sem placerat nibh, at convallis nunc tortor in nisl. Praesent lorem sem, malesuada nec sagittis quis, accumsan vel ante. Curabitur elementum augue id tempor posuere."
     >
-      <Box mb={4}>
-        <Container>
-          <Typography variant="h2" component="h1">
+      <Box py={4}>
+        <Container component="section">
+          <Typography component="h1" align="center" className={classes.title}>
             Contacto
           </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Typography>
-                Obcaecati repellendus blanditiis tenetur facilis nam architecto
-                iure aspernatur perferendis temporibus id ex, neque rem
-                voluptate quis tempora esse earum alias voluptates.
-              </Typography>
+          <Typography
+            component="div"
+            align="center"
+            className={classes.phoneNumber}
+          >
+            +58 123 45-67
+          </Typography>
+          <Typography component="div" align="center">
+            Avenida Luis Roche, Caracas 1060, Miranda
+          </Typography>
+        </Container>
+      </Box>
+      <section id="map" className={classes.map}>
+        <div className={classes.mapWrapper}>
+          <div className={classes.mapContent}>
+            <Typography component="div" variant="h2">
+              Mapa
+            </Typography>
+          </div>
+        </div>
+      </section>
+      <Box py={10} minHeight="10vmax" className={classes.bgLight}>
+        <Container component="section" id="rrss">
+          <h2 className={classes.rsTitle}>Nuestras redes.</h2>
+          <Grid
+            container
+            spacing={2}
+            justify="center"
+            className={classes.rsContent}
+          >
+            <Grid item>
+              <IconButton
+                className={classes.icon}
+                disableRipple
+                aria-label="Cuenta de whatsapp"
+              >
+                <FontAwesomeIcon size="lg" icon={faWhatsapp} />
+              </IconButton>
             </Grid>
-            <Grid item xs={12} md={6}>
+            <Grid item>
+              <IconButton
+                className={classes.icon}
+                disableRipple
+                aria-label="Cuenta de instagram"
+              >
+                <FontAwesomeIcon size="lg" icon={faInstagram} />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton
+                className={classes.icon}
+                disableRipple
+                aria-label="Cuenta de facebook"
+              >
+                <FontAwesomeIcon size="lg" icon={faFacebook} />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton
+                className={classes.icon}
+                disableRipple
+                aria-label="Cuenta de twitter"
+              >
+                <FontAwesomeIcon size="lg" icon={faTwitter} />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton
+                className={classes.icon}
+                disableRipple
+                aria-label="Canal de youtube"
+              >
+                <FontAwesomeIcon size="lg" icon={faYoutube} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+
+      <Box py={8} className={classes.bgDark}>
+        <Container>
+          <Box mb={4}>
+            <Typography variant="h2" align="center">
+              Contáctanos
+            </Typography>
+          </Box>
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={7}>
               <ContactForm />
             </Grid>
-            <Grid item xs={12} md={6}>
-              <Paper variant="outlined" className={classes.map}>
-                <div className={classes.mapWrapper}>
-                  <div className={classes.mapContent} />
-                </div>
-              </Paper>
+            <Grid item xs={12} md={5}>
+              <div className={classes.info}>
+                <Typography
+                  variant="h4"
+                  component="span"
+                  align="center"
+                  gutterBottom
+                >
+                  <strong>+58 123 45-67</strong>
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="textSecondary"
+                  component="span"
+                  align="center"
+                  gutterBottom
+                >
+                  Horario: 10:00 – 19:00
+                </Typography>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  align="center"
+                  gutterBottom
+                >
+                  <strong>Dirección</strong> Avenida Luis Roche, Caracas 1060,
+                  Miranda
+                </Typography>
+                <Typography
+                  variant="body1"
+                  component="span"
+                  align="center"
+                  gutterBottom
+                >
+                  <strong>Email</strong> contacto@avilatek.com
+                </Typography>
+              </div>
             </Grid>
           </Grid>
         </Container>
