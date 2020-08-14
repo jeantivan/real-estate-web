ee6b7b8180c4fa7ea10c2e92ca4f353e8c443a02
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2020-08-13 20:36:39 -0400

    Nuevo diseño de footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,110 @@
 import { makeStyles } from "@material-ui/core/styles";
-import { Typography, Link, Grid } from "@material-ui/core/";
+import {
+  Link,
+  Grid,
+  Container,
+  Typography,
+  IconButton,
+} from "@material-ui/core/";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHeart,
   faMapMarkerAlt,
-  faPhone,
+  faPhoneAlt,
   faEnvelope,
   faCopyright,
 } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+import NextLink from "next/link";
 
 import Logo from "./Logo";
-import RRSS from "./RRSS";
+import Newsletter from "./Newsletter";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
-    backgroundColor: "#20232A",
+    backgroundColor: "#24324a",
     color: "white",
   },
-  container: {
-    maxWidth: 1920,
-    width: "100%",
-    margin: "0 auto",
-    padding: theme.spacing(4, 2),
-    [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(4),
-    },
-    [theme.breakpoints.up("xl")]: {
-      padding: theme.spacing(4, 10),
-    },
+  bgDark: {
+    backgroundColor: "#1d293e",
   },
-  me: {
-    ...theme.typography.body2,
-    padding: theme.spacing(2, 0),
-    //borderTop: `1px solid #130554`,
-    maxWidth: 1920,
-    width: "100%",
-    margin: "0 auto",
-    backgroundColor: "#181a1e",
+  container: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  gridItem: {
+    padding: theme.spacing(2),
+  },
+  subFooter: {
+    padding: theme.spacing(3),
+    color: "#8a99b3",
+    fontSize: "0.8rem",
+    display: "flex",
+  },
+  copyright: {
+    flexGrow: 1,
+    display: "flex",
+    alignItems: "center",
+  },
+  logo: {
+    marginBottom: theme.spacing(2),
+  },
+  heading: {
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: theme.spacing(4),
+    },
   },
   text: {
-    margin: theme.spacing(2, 0),
+    color: "#8a99b3",
+    fontSize: "0.8rem",
+    display: "inline-flex",
+    alignItems: "center",
+    lineHeight: 1.2,
+    marginBottom: theme.spacing(2),
+  },
+  link: {
+    ...theme.typography.body2,
+    color: "#8a99b3",
+
+    marginBottom: theme.spacing(2),
+    transition: theme.transitions.create(["color", "textDecoration"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+
+    "&:hover": {
+      color: "white",
+    },
   },
   icon: {
+    width: 12,
+    height: 12,
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
     marginRight: theme.spacing(2),
+  },
+  rs: {
+    marginTop: theme.spacing(-1.5),
+  },
+  social: {
+    color: "#8a99b3",
+    transition: theme.transitions.create("color", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+
+    "&:hover": {
+      color: "white",
+    },
   },
   heart: {
     color: "#f44336",
@@ -63,56 +124,181 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Footer = () => {
+export default function Footer() {
   const classes = useStyles();
 
   return (
     <footer className={classes.root}>
-      <Grid container justify="space-between" className={classes.container}>
-        <Grid item md={9} lg={10}>
-          <Logo type="footer" />
-          <Typography variant="overline" display="block" gutterBottom>
-            <span role="img" aria-label="copyright">
-              <FontAwesomeIcon icon={faCopyright} className={classes.icon} />
-            </span>
-            {new Date().getFullYear()} | Todos los derechos reservados.
-          </Typography>
-          <Typography variant="caption" display="block" gutterBottom>
-            <FontAwesomeIcon icon={faMapMarkerAlt} className={classes.icon} />
-            Centro Sambil Caracas Av. Libertador, Caracas 1064.
-          </Typography>
-          <Typography variant="caption" display="block" gutterBottom>
-            <FontAwesomeIcon icon={faPhone} className={classes.icon} />
-            +58 424 123 45-67
-          </Typography>
-          <Typography variant="caption" display="block" gutterBottom>
-            <FontAwesomeIcon icon={faEnvelope} className={classes.icon} />
-            contacto@avilatek.com
-          </Typography>
-        </Grid>
-        <Grid item md={3} lg={2}>
-          <RRSS type="dark" />
-        </Grid>
-      </Grid>
-      <Grid container justify="center" className={classes.me}>
-        <Grid item>
-          Creado con{" "}
-          <span>
-            <FontAwesomeIcon icon={faHeart} className={classes.heart} />
-          </span>{" "}
-          por{" "}
-          <Link
-            target="_blank"
-            href="https://github.com/jptivan53"
-            rel="noopener noreferrer"
-            color="primary"
+      <Container className={classes.container}>
+        <Grid container justify="space-between">
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            container
+            direction="column"
+            className={classes.gridItem}
           >
-            JEAN TIVAN.
-          </Link>
+            <Typography variant="h6" className={classes.heading}>
+              Nosotros
+            </Typography>
+            <Logo type="footer" className={classes.logo} />
+            <Typography className={classes.text}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
+              ipsa ut labore ducimus similique voluptates tempora deleniti
+              minima quisquam odit?
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={2}
+            container
+            direction="column"
+            className={classes.gridItem}
+          >
+            <Typography variant="h6" className={classes.heading}>
+              Navega
+            </Typography>
+            <NextLink href="/" passHref>
+              <Link className={classes.link}>Inicio</Link>
+            </NextLink>
+            <NextLink href="/inmuebles" passHref>
+              <Link className={classes.link}>Inmuebles</Link>
+            </NextLink>
+            <NextLink href="/nosotros" passHref>
+              <Link className={classes.link}>Nosotros</Link>
+            </NextLink>
+            <NextLink href="/contacto" passHref>
+              <Link className={classes.link}>Contacto</Link>
+            </NextLink>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            container
+            direction="column"
+            className={classes.gridItem}
+          >
+            <Typography variant="h6" className={classes.heading}>
+              Contáctanos
+            </Typography>
+
+            <div className={classes.text}>
+              <span role="img" aria-label="ubicacion" className={classes.icon}>
+                <FontAwesomeIcon icon={faMapMarkerAlt} />
+              </span>
+              Centro Sambil Caracas, Av. Libertador, Caracas 1064.
+            </div>
+            <div className={classes.text}>
+              <span role="img" aria-label="telefono" className={classes.icon}>
+                <FontAwesomeIcon icon={faPhoneAlt} />
+              </span>
+              +58 424 123 45-67
+            </div>
+            <div className={classes.text}>
+              <span role="img" aria-label="telefono" className={classes.icon}>
+                <FontAwesomeIcon icon={faPhoneAlt} />
+              </span>
+              +58 212 000 00-00
+            </div>
+            <div className={classes.text}>
+              <span role="img" aria-label="email" className={classes.icon}>
+                <FontAwesomeIcon icon={faEnvelope} />
+              </span>
+              contacto@avilatek.com
+            </div>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={3}
+            container
+            direction="column"
+            className={classes.gridItem}
+          >
+            <Typography variant="h6" className={classes.heading}>
+              Siguenos
+            </Typography>
+            <div className={classes.rs}>
+              <IconButton
+                className={classes.social}
+                href="#"
+                edge="start"
+                disableRipple
+                aria-label="Cuenta de instagram"
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </IconButton>
+              <IconButton
+                className={classes.social}
+                href="#"
+                disableRipple
+                aria-label="Cuenta de facebook"
+              >
+                <FontAwesomeIcon icon={faFacebook} />
+              </IconButton>
+              <IconButton
+                className={classes.social}
+                href="#"
+                disableRipple
+                aria-label="Cuenta de twitter"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </IconButton>
+              <IconButton
+                className={classes.social}
+                href="#"
+                edge="end"
+                disableRipple
+                aria-label="Canal de youtube"
+              >
+                <FontAwesomeIcon icon={faYoutube} />
+              </IconButton>
+            </div>
+            <Typography variant="h6" gutterBottom>
+              Promociones
+            </Typography>
+            <Typography className={classes.text}>
+              Recibe notificaciones cuando se publique un nuevo inmueble.
+            </Typography>
+            <Newsletter />
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
+      <div className={classes.bgDark}>
+        <Container>
+          <div className={classes.subFooter}>
+            <div className={classes.copyright}>
+              <span role="img" aria-label="ubicacion" className={classes.icon}>
+                <FontAwesomeIcon icon={faCopyright} />
+              </span>
+              {new Date().getFullYear()} Avilatek | Todos los derechos
+              reservados
+            </div>
+            <div className={classes.me}>
+              Creado con{" "}
+              <span>
+                <FontAwesomeIcon icon={faHeart} className={classes.heart} />
+              </span>{" "}
+              por{" "}
+              <Link
+                target="_blank"
+                href="https://github.com/jptivan53"
+                rel="noopener noreferrer"
+                color="primary"
+              >
+                JEAN TIVAN.
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </div>
     </footer>
   );
-};
-
-export default Footer;
+}
