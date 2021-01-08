7790ce79416de553ff40d080ac4b26ea3c2cf0ab
jptivan53 <jptivan53@gmail.com>
2021-01-08 19:27:28 -0400

    Thumbnails de las imagenes del inmueble

---
+++ src/components/Gallery/Thumbnail.js
@@ -0,0 +1,37 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Image from "next/image";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+    width: "100%",
+    height: 0,
+    overflow: "hidden",
+    paddingTop: "56.25%",
+    background: theme.palette.grey[600],
+    cursor: "pointer",
+  },
+  container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: "100%",
+    width: "100%",
+  },
+  active: {
+    outline: `2px solid ${theme.palette.primary.main}`,
+  },
+}));
+export function Thumbnail({ imagen, active }) {
+  const classes = useStyles();
+  return (
+    <div className={clsx(classes.root, { [classes.active]: active })}>
+      <div className={classes.container}>
+        <Image alt={imagen.alt} src={imagen.url} layout="fill" />
+      </div>
+    </div>
+  );
+}
