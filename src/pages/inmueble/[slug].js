09009949917d428ab6b444b7f689993cfe1f54d9
jptivan53 <jptivan53@gmail.com>
2021-01-08 17:09:00 -0400

    Restructuración de la pagina, se usa el componente Inmueble Header

--- src/pages/inmueble/[slug].js
+++ src/pages/inmueble/[slug].js
@@ -24,27 +24,15 @@ import {
   getAllInmueblesSlug,
   getInmueblesSimilares,
 } from "../../lib/api";
+import { InmuebleHeader } from "../../layouts/inmueble-header";
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    //fontWeight: "semibold",
-    //color: theme.palette.primary.dark,
-  },
-  price: {
-    fontWeight: 700,
-    color: theme.palette.primary.dark,
-  },
   gallery: {
     minWidth: "100%",
     borderRadius: theme.spacing(2),
     [theme.breakpoints.up("md")]: {
       minWidth: 0,
     },
-  },
-  location: {
-    display: "inline-flex",
-    alignItems: "center",
-    color: theme.palette.primary.main,
   },
   description: {
     fontWeight: 700,
@@ -66,9 +54,6 @@ const useStyles = makeStyles((theme) => ({
       ...theme.typography.body1,
       maxWidth: "100%",
     },
-  },
-  icon: {
-    marginRight: theme.spacing(1),
   },
   masInmuebles: {
     padding: theme.spacing(2, 0),
@@ -105,101 +90,69 @@ export default function Inmueble({ inmueble, inmueblesSimilares }) {
       titulo={RichText.asText(inmueble.titulo)}
       descripcion={inmueble.descCorta}
     >
-      <Container fixed maxWidth="md">
+      <Container fixed maxWidth="lg">
         <Box py={4}>
-          <Grid container spacing={3} component="article">
-            <Grid
-              item
-              lg={12}
-              xs={12}
-              id="titulo-de-la-propiedad"
-              component="section"
-              container
-            >
-              <Grid item style={{ flex: 1 }}>
-                <Typography
-                  variant="h4"
-                  component="h1"
-                  className={classes.title}
+          <Grid container justify="center">
+            <Grid item xs={12} md={10}>
+              <Box component="article">
+                <InmuebleHeader
+                  titulo={inmueble.titulo}
+                  ubiaprox={inmueble.ubiaprox}
+                  precio={inmueble.precio}
+                />
+                <Box
+                  component="section"
+                  className={classes.gallery}
+                  id="galeria-del-inmueble"
                 >
-                  {RichText.asText(inmueble.titulo)}
-                </Typography>
-                <div className={classes.location}>
-                  <Typography
-                    variant="body1"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    <span className={classes.icon}>
-                      <FontAwesomeIcon icon={faMapMarkerAlt} />
-                    </span>
-                    {inmueble.ubiaprox}
-                  </Typography>
-                </div>
-              </Grid>
-              <Grid item>
-                <Typography
-                  variant="h3"
-                  component="h4"
-                  className={classes.price}
-                >
-                  <sup>$</sup>
-                  {inmueble.precio}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid
-              component="section"
-              item
-              xs={12}
-              lg={12}
-              className={classes.gallery}
-            >
-              <Gallery imagenes={inmueble.imagenes} />
-            </Grid>
-            {/* <Grid item xs={12} lg={4} component="section">
+                  <Gallery imagenes={inmueble.imagenes} />
+                </Box>
+                {/* <Grid item xs={12} lg={4} component="section">
             <InmuebleInformation {...inmuebleInfo} agent={inmueble.agent} />
           </Grid> */}
-            <Grid
-              item
-              xs={12}
-              lg={8}
-              component="section"
-              id="descripcion-de-la-propiedad"
-            >
-              <Typography
-                variant="h6"
-                component="h3"
-                className={classes.description}
-                gutterBottom
-              >
-                Descripción.
-              </Typography>
-              <div className={classes.html}>
-                <RichText render={inmueble.descripcion} />
-              </div>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              lg={4}
-              component="section"
-              id="descripcion-de-la-propiedad"
-            >
-              <Paper elevation={0} variant="outlined">
-                <Box p={2}>
+                <Grid
+                  item
+                  xs={12}
+                  lg={8}
+                  component="section"
+                  id="descripcion-de-la-propiedad"
+                >
                   <Typography
                     variant="h6"
                     component="h3"
                     className={classes.description}
                     gutterBottom
                   >
-                    Caracteristicas.
+                    Descripción.
                   </Typography>
-                </Box>
-              </Paper>
+                  <div className={classes.html}>
+                    <RichText render={inmueble.descripcion} />
+                  </div>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  lg={4}
+                  component="section"
+                  id="descripcion-de-la-propiedad"
+                >
+                  <Paper elevation={0} variant="outlined">
+                    <Box p={2}>
+                      <Typography
+                        variant="h6"
+                        component="h3"
+                        className={classes.description}
+                        gutterBottom
+                      >
+                        Caracteristicas.
+                      </Typography>
+                    </Box>
+                  </Paper>
+                </Grid>
+              </Box>
             </Grid>
           </Grid>
+
           {/* <div className={classes.masInmuebles}>
             <Grid container justify="space-between" spacing={3}>
               <Grid item>
