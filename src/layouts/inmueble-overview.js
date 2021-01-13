0fe155f239df9866c4ac13fd27b0a927e3472a91
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2021-01-12 22:24:30 -0400

    Se muestran las características principales y la descripción del inmueble.

--- src/layouts/inmueble-overview.js
+++ src/layouts/inmueble-overview.js
@@ -1,37 +1,123 @@
-import { Paper, Box, Typography } from "@material-ui/core";
+import { Paper, Divider, Typography, Grid, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { RichText } from "prismic-reactjs";
 
 const useStyles = makeStyles((theme) => ({
-  description: {
+  section: {
+    padding: theme.spacing(2),
+  },
+  sectionTitle: {
+    ...theme.typography.h6,
     fontWeight: 700,
-    position: "relative",
-    display: "inline-block",
-
-    "&:after": {
-      position: "absolute",
-      bottom: 0,
-      left: 0,
-      content: "''",
-      width: "45%",
-      height: 2,
-      backgroundColor: theme.palette.primary.light,
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+  content: {
+    ...theme.typography.h6,
+  },
+  description: {
+    "& > p": {
+      ...theme.typography.body1,
+    },
+    "& > p:not(:last-child)": {
+      marginTop: 0,
+      marginBottom: theme.spacing(2),
     },
   },
 }));
 
-export function InmuebleOverview() {
+export function InmuebleOverview({
+  id,
+  tipo,
+  area,
+  habitaciones,
+  baños,
+  estacionamientos,
+  antiguedad,
+  descripcion,
+}) {
+  const classes = useStyles();
+
   return (
-    <Paper component="section" elevation={0} variant="outlined">
-      <Box p={2}>
-        <Typography
-          variant="h6"
-          component="h3"
-          className={classes.description}
-          gutterBottom
-        >
-          Caracteristicas.
-        </Typography>
-      </Box>
-    </Paper>
+    <Grid item xs={12} md={7}>
+      <Paper elevation={0} className={classes.section}>
+        <Box component="section" id="caracteristicas-del-inmueble" mb={3}>
+          <Grid container justify="space-between" alignItems="baseline">
+            <Grid item>
+              <Typography component="h3" className={classes.sectionTitle}>
+                Caracteristicas.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" color="textSecondary">
+                <strong>ID del Inmueble: </strong>
+                {id}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <Grid container spacing={2}>
+            <Grid item xs lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Tipo
+              </Typography>
+              <Typography className={classes.content}>{tipo}</Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Area
+              </Typography>
+              <Typography className={classes.content}>
+                {area + " "}
+                <span>
+                  m<sup>2</sup>
+                </span>
+              </Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Habitaciones
+              </Typography>
+              <Typography className={classes.content}>
+                {habitaciones}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Baños
+              </Typography>
+              <Typography className={classes.content}>{baños}</Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Estacionamientos
+              </Typography>
+              <Typography className={classes.content}>
+                {estacionamientos}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={3}>
+              <Typography variant="caption" gutterBottom>
+                Antiguedad
+              </Typography>
+              <Typography className={classes.content}>{antiguedad}</Typography>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box component="section">
+          <Typography
+            component="h3"
+            gutterBottom
+            className={classes.sectionTitle}
+          >
+            Descripción.
+          </Typography>
+          <div className={classes.description}>
+            <RichText render={descripcion} />
+          </div>
+        </Box>
+      </Paper>
+    </Grid>
   );
 }
