16b83953fe411d872928373db22c431e3cff2d21
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2021-02-09 18:10:35 -0400

    Movido a carpeta utils

---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: {
+      main: "#FA4400",
+    },
+/*     background: {
+      default: "#fff",
+    }, */
+  },
+  typography: {
+    fontFamily: [
+      '"Montserrat"',
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+    ].join(","),
+  },
+});
+
+export default responsiveFontSizes(theme);
