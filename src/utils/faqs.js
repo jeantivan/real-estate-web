93b1147ca81d47dc3529ec82c12c3f9526bda619
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2021-02-09 18:13:08 -0400

    Preguntas Frecuentes

---
+++ src/utils/faqs.js
@@ -0,0 +1,27 @@
+export const faqs = [
+  {
+    question: "¿Praesent odio lectus, laoreet ut nisl aliquam, pharetra?",
+    answer:
+      "In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
+  },
+  {
+    question: "¿Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo?",
+    answer:
+      "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+  },
+  {
+    question: "Vivamus ut dolor nec purus eleifend elementum?",
+    answer:
+      "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+  },
+  {
+    question: "¿Vivamus pulvinar cursus nulla, malesuada semper sem tincidunt?",
+    answer:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+  },
+  {
+    question: "¿Aliquam efficitur vitae lorem ut ornare. Proin laoreet?",
+    answer:
+      "Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+  },
+];
