045ed162ce60ee2314d2cf53514cc8659e6d6e2c
Jean Paul Tivan <49886458+jptivan53@users.noreply.github.com>
2021-02-11 19:04:25 -0400

    ...

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,6 +3,7 @@ import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
 import { makeStyles } from "@material-ui/core/styles";
 import clsx from "clsx";
+import { testimonials } from "@/utils/testimonials";
 
 const useStyles = makeStyles((theme) => ({
   testimonials: {
@@ -10,13 +11,13 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "100vw",
   },
   testimonialSlide: {
-    padding: theme.spacing(3, 2),
-    [theme.breakpoints.up("sm")]: {
-      padding: theme.spacing(4),
-    },
-    [theme.breakpoints.up("md")]: {
-      padding: theme.spacing(4, 6),
-    },
+    // padding: theme.spacing(3, 2),
+    // [theme.breakpoints.up("sm")]: {
+    //   padding: theme.spacing(4),
+    // },
+    // [theme.breakpoints.up("md")]: {
+    //   padding: theme.spacing(4, 6),
+    // },
   },
   dots: {
     padding: theme.spacing(1),
@@ -40,37 +41,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.primary.dark,
   },
 }));
-
-const testimonials = [
-  {
-    client: "Cecilio Manzanares",
-    type: "Comprador",
-    picture: "/images/male-1.jpg",
-    text:
-      "Etiam condimentum mauris viverra elit ullamcorper, in pellentesque lectus faucibus.Nullam sodales, turpis quis luctus euismod, magna sem egestas ipsum, id posuere ex eros sed.",
-  },
-  {
-    client: "Daila Zapata Jiménez",
-    type: "Vendedora",
-    picture: "/images/female-1.jpg",
-    text:
-      "Fusce finibus, orci a imperdiet semper, dui justo vestibulum orci, id ornare arcu tellus ut metus. Integer gravida nibh neque, ac lobortis neque ornare id. Phasellus ac pharetra orci, at tristique nibh. Aliquam ac augue.",
-  },
-  {
-    client: "Francisco Cardona Cadena",
-    type: "Vendedor",
-    picture: "/images/male-2.jpg",
-    text:
-      "Etiam mollis posuere dui, porta dictum neque. Etiam posuere aliquam quam, non facilisis orci euismod quis. Nunc in ullamcorper ipsum. Aenean dapibus libero in maximus.",
-  },
-  {
-    client: "Josefina Morales",
-    type: "Compradora",
-    picture: "/images/female-2.jpg",
-    text:
-      "Duis non orci feugiat, bibendum metus sit amet, dignissim ligula. Donec vel pellentesque quam. Sed vel risus nibh. Cras ipsum tortor, tristique ac magna et, scelerisque aliquet ante. Sed pulvinar diam in enim euismod, vel.",
-  },
-];
 
 const AutoSwipeableViews = autoPlay(SwipeableViews);
 
