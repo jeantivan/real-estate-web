1353d0539b1745261a34ede634c3a093d1479d7f
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-17 20:45:41 -0400

    Proceso de actualización de la sección Redes para la pagina Contacto

--- src/layouts/contacto/redes.js
+++ src/layouts/contacto/redes.js
@@ -1,5 +1,16 @@
 import { Facebook, Instagram, Twitter, WhatsApp } from "@mui/icons-material";
-import { Container, Grid, IconButton, Button } from "@mui/material";
+import {
+  Container,
+  Grid,
+  IconButton,
+  Button,
+  Typography,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import React from "react";
 import makeStyles from "@mui/styles/makeStyles";
 
@@ -11,16 +22,22 @@ const useStyles = makeStyles((theme) => ({
     // },
   },
   bgLight: {
-    backgroundColor: "#fafafa",
+    backgroundColor: "white",
   },
   rsHeading: {
     ...theme.typography.h5,
     fontWeight: "bold",
-    textAlign: "center",
     margin: 0,
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(3),
   },
-  icon: {
+  list: {
+    listStyle: "none",
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  listItem: {
+    fontSize: theme.spacing(2.5),
+    display: "flex",
     color: theme.palette.text.secondary,
     transition: theme.transitions.create(["color", "transform"], {
       easing: theme.transitions.easing.sharp,
@@ -32,62 +49,64 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.primary.main,
     },
   },
+  listIcon: {
+    minWidth: 56,
+  },
 }));
 export function RedesSection() {
   const classes = useStyles();
   return (
     <section id="rrss" className={classes.bgLight}>
       <Container className={classes.container}>
-        <h2 className={classes.rsHeading}>
-          Siguenos en nuestras redes sociales.
-        </h2>
-        <Grid
-          container
-          spacing={2}
-          justifyContent="center"
-          className={classes.rsContent}
+        <h2 className={classes.rsHeading}>Nuestras redes sociales.</h2>
+        <Typography
+          variant="body1
+        "
         >
-          <Grid item>
-            <Button
-              disableRipple
-              aria-label="Cuenta de instagram"
-              size="large"
-              startIcon={<Instagram fontSize="large" />}
-            >
-              Instagram
-            </Button>
-          </Grid>
-          <Grid item>
-            <IconButton
-              className={classes.icon}
-              disableRipple
-              aria-label="Cuenta de facebook"
-              size="large"
-            >
-              <Facebook fontSize="large" />
-            </IconButton>
-          </Grid>
-          <Grid item>
-            <IconButton
-              className={classes.icon}
-              disableRipple
-              aria-label="Cuenta de twitter"
-              size="large"
-            >
-              <Twitter fontSize="large" />
-            </IconButton>
-          </Grid>
-          <Grid item>
-            <IconButton
-              className={classes.icon}
-              disableRipple
-              aria-label="Canal de youtube"
-              size="large"
-            >
-              <WhatsApp fontSize="large" />
-            </IconButton>
-          </Grid>
-        </Grid>
+          Enterate de nuestros nuevos inmbuebles publicados, siguenos
+        </Typography>
+        <ul className={classes.list}>
+          <li className={classes.listItem}>
+            <div className={classes.listIcon}>
+              <Instagram />
+            </div>
+            <div>@avilatekinmobiliaria</div>
+          </li>
+        </ul>
+        <List>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <Instagram />
+              </ListItemIcon>
+              <ListItemText primary="@avilatekinmobiliaria" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <Facebook />
+              </ListItemIcon>
+              <ListItemText primary="Avilatek Inmobiliaria" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <Twitter />
+              </ListItemIcon>
+              <ListItemText primary="@avilatekinmobiliaria" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <WhatsApp />
+              </ListItemIcon>
+              <ListItemText primary="+58 424 1234567" />
+            </ListItemButton>
+          </ListItem>
+        </List>
       </Container>
     </section>
   );
