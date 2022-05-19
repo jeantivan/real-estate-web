4de9f933c677400510a1aca0d919d7775ef61424
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-18 23:11:03 -0400

    Conflicto resuelto

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -2,59 +2,9 @@ import { Layout } from "@/components";
 import { Container, Typography } from "@mui/material";
 import { UbicacionSection } from "@/layouts/contacto/ubicacion";
 import { RedesSection } from "@/layouts/contacto/redes";
-<<<<<<< HEAD
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(2),
-    // [theme.breakpoints.up("sm")]: {
-    //   padding: theme.spacing(4, 2),
-    // },
-  },
-  titleContainer: {
-    paddingTop: theme.spacing(2),
-  },
-  title: {
-    ...theme.typography.body1,
-
-    fontWeight: 800,
-    textTransform: "uppercase",
-  },
-  bgDark: {
-    backgroundColor: "#000",
-    color: "white",
-  },
-  mainHeading: {
-    ...theme.typography.h6,
-    fontWeight: 500,
-    textTransform: "uppercase",
-    letterSpacing: 2,
-  },
-  contactHeading: {
-    ...theme.typography.h2,
-    textAlign: "center",
-    margin: 0,
-    marginBottom: theme.spacing(2),
-  },
-  phoneNumber: {
-    ...theme.typography.h3,
-    fontWeight: "bold",
-  },
-  info: {
-    backgroundColor: "white",
-    color: "#000",
-    height: "100%",
-    padding: theme.spacing(3, 2),
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-}));
-=======
 import { FormularioSection } from "@/layouts/contacto/formulario";
 import { NewsletterSection } from "@/layouts/contacto/newsletter";
->>>>>>> local
 
 export default function Contacto() {
   return (
