1a09fcb8a19a36d2d727510fbe92e2c35143084e
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-20 20:06:29 -0400

    Rewrite para las páginas "/" y "/inmuebles"

--- next.config.js
+++ next.config.js
@@ -3,13 +3,25 @@ module.exports = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+      use: ["@svgr/webpack"],
+    });
 
-    return config
+    return config;
   },
   images: {
     domains: ["images.prismic.io"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+      {
+        source: "/inmuebles",
+        destination: "/inmuebles/1",
+      },
+    ];
   },
   swcMinify: true,
 };
