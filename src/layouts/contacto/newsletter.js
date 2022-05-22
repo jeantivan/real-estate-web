6a418a6b71a67760a4d09bf83008805e40997c98
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-22 19:56:12 -0400

    Sección newsletter para la pág de contacto

--- src/layouts/contacto/newsletter.js
+++ src/layouts/contacto/newsletter.js
@@ -1,5 +1,41 @@
 import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { Newsletter } from "@/components/";
 
 export function NewsletterSection() {
-  return <section id="section"></section>;
+  return (
+    <Box
+      id="newsletter-section"
+      component="section"
+      sx={{ backgroundColor: "primary.main", py: 4 }}
+    >
+      <Container>
+        <Grid container alignItems="flex-end" spacing={2}>
+          <Grid item md={8} sx={{ marginBottom: { xs: 3, sm: 0 } }}>
+            <Typography
+              sx={(theme) => ({
+                ...theme.typography.h5,
+                fontWeight: "bold",
+                margin: 0,
+                marginBottom: theme.spacing(3),
+                color: theme.palette.primary.contrastText,
+              })}
+            >
+              Enterate
+            </Typography>
+
+            <Typography
+              sx={(theme) => ({ color: theme.palette.primary.contrastText })}
+            >
+              Recibe en tu correo nuestras últimas novedades y promociones,
+              suscribete a nuestro newsletter
+            </Typography>
+          </Grid>
+          <Grid item md={4} container alignItems="flex-start">
+            <Newsletter isLight />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
 }
