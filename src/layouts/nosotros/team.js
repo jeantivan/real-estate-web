42c3b2a7a06f80679b53266a79a28a3c3942fbfe
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-25 19:37:10 -0400

    Actualizaciones en la UI de las sección "Team"

--- src/layouts/nosotros/team.js
+++ src/layouts/nosotros/team.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Container, Box, Typography, Grid, Button } from "@mui/material";
+import {
+  Container,
+  Box,
+  Typography,
+  Grid,
+  Button,
+  IconButton,
+} from "@mui/material";
 import Image from "next/image";
 import { EmailOutlined, WhatsappOutlined } from "@mui/icons-material";
 
@@ -8,23 +15,29 @@ export function TeamSection({ agents }) {
   return (
     <Box component="section" id="team" backgroundColor="white">
       <Container sx={{ py: 6 }}>
-        <Typography
-          component="h2"
-          variant="h3"
-          sx={(theme) => ({
-            ...theme.typography.h4,
-            fontWeight: "bold",
-            mb: 3,
-            textAlign: "center",
-          })}
-        >
-          Conoce a nuestros equipo
-        </Typography>
+        <Box mb={3}>
+          <Typography
+            component="h2"
+            variant="h3"
+            sx={(theme) => ({
+              ...theme.typography.h4,
+              fontWeight: "bold",
+              mb: 1,
+              textAlign: "center",
+            })}
+          >
+            Conoce a nuestros equipo
+          </Typography>
+          <Typography textAlign="center">
+            Ellos te ayudarán a conseguir el inmueble de tus sueños
+          </Typography>
+        </Box>
+
         <Grid container justifyContent="center">
-          <Grid item container xs={12} sm={8} rowSpacing={3} columnSpacing={3}>
+          <Grid item container xs={12} sm={8} rowSpacing={4} columnSpacing={4}>
             {agents.map(({ data, id }) => (
               <Grid item key={id} xs={12} sm={6}>
-                <Box p={2}>
+                <Box>
                   <Box backgroundColor="#4a4848" borderRadius={2} width="100%">
                     <Image
                       src={data.picture.url}
@@ -35,42 +48,54 @@ export function TeamSection({ agents }) {
                       style={{ borderRadius: 8 }}
                     />
                   </Box>
-                  <Box py={1}>
-                    <Typography
-                      sx={(theme) => ({
-                        ...theme.typography.h6,
-                        fontWeight: "bold",
-                        lineHeight: 1,
-                      })}
+                  <Grid container spacing={1} pt={2}>
+                    <Grid item xs sm={12}>
+                      <Typography
+                        sx={(theme) => ({
+                          ...theme.typography.h6,
+                          fontWeight: "bold",
+                          lineHeight: 1,
+                        })}
+                      >
+                        {data.name}
+                      </Typography>
+                      <Typography
+                        color="text.secondary"
+                        textTransform="uppercase"
+                        variant="overline"
+                      >
+                        Asesor(a) inmobiliario
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      xs="auto"
+                      sm={6}
+                      container
+                      justifyContent="center"
                     >
-                      {data.name}
-                    </Typography>
-                    <Typography
-                      color="text.secondary"
-                      textTransform="uppercase"
-                      variant="overline"
-                    >
-                      Asesor(a) inmobiliario
-                    </Typography>
-                  </Box>
-                  <Grid container spacing={1}>
-                    <Grid item xs={6}>
-                      <Button
-                        variant="text"
-                        fullWidth
+                      <IconButton
+                        color="inherit"
+                        size="large"
                         aria-label={`Whatsapp del asesor: ${data.name}`}
                       >
                         <WhatsappOutlined />
-                      </Button>
+                      </IconButton>
                     </Grid>
-                    <Grid item xs={6}>
-                      <Button
-                        variant="text"
-                        fullWidth
-                        aria-label={`Whatsapp del asesor: ${data.name}`}
+                    <Grid
+                      item
+                      xs="auto"
+                      sm={6}
+                      container
+                      justifyContent="center"
+                    >
+                      <IconButton
+                        color="inherit"
+                        size="large"
+                        aria-label={`Email del asesor: ${data.name}`}
                       >
                         <EmailOutlined />
-                      </Button>
+                      </IconButton>
                     </Grid>
                     <Grid item xs={12}>
                       <Button variant="contained" fullWidth disableElevation>
