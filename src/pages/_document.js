3b239a28a34d5196a0d01d3c544e3220f3f816b8
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-26 13:18:57 -0400

    El font weight coincide con las propiedades del tema de mui

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
 
