f906ecfa7945f47cd3d82bb09280e7073eae3107
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-27 21:56:45 -0400

    Constantes para el proyectos

---
+++ src/utils/constants.js
@@ -0,0 +1,88 @@
+import realEstateImg from "@/images/services/real-estate.jpg";
+import findHouseImg from "@/images/services/find-house.jpg";
+import marketAnalysisImg from "@/images/services/market-analysis.jpg";
+import goalsImg from "@/images/services/goals.jpg";
+import legalAdviceImg from "@/images/services/legal-advice.jpg";
+
+export const SERVICES = [
+  {
+    title: "Comercialización de inmuebles",
+    text: "Et tempus semper est quam pharetra magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus",
+    img: realEstateImg,
+  },
+  {
+    title: "Encontramos tu inmueble ideal",
+    text: "Elementum in hac habitasse platea dictumst morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet eros suspendisse accumsan tortor quis turpis sed ante",
+    img: findHouseImg,
+  },
+  {
+    title: "Asesoría legal",
+    text: "Morbi odio odio elementum eu interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec",
+    img: legalAdviceImg,
+  },
+  {
+    title: "Análisis del mercado",
+    text: "Magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit",
+    img: marketAnalysisImg,
+  },
+  {
+    title: "Alcanzamos tus metas",
+    text: "Suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus etiam vel augue vestibulum rutrum rutrum neque",
+    img: goalsImg,
+  },
+];
+
+export const FAQS = [
+  {
+    question: "¿Praesent odio lectus, laoreet ut nisl aliquam, pharetra?",
+    answer:
+      "Nam congue mi eros, quis vestibulum est laoreet eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer sit amet tellus libero. Ut blandit risus suscipit nibh congue dignissim. Suspendisse dolor nibh, porttitor ut augue a, auctor elementum sapien. Aliquam erat volutpat. In eleifend ornare.",
+  },
+  {
+    question: "¿Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo?",
+    answer:
+      "Pellentesque pellentesque auctor commodo. Phasellus interdum dui nisi, eget tristique tellus varius cursus. Donec ornare semper venenatis. Duis vulputate id massa vitae pretium. Sed interdum enim non mattis cursus. Etiam vel tempus mauris. Quisque et vulputate massa. Vestibulum eu metus.",
+  },
+  {
+    question: "Vivamus ut dolor nec purus eleifend elementum?",
+    answer:
+      "Proin quis ipsum vel velit dictum molestie at blandit ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit erat, tincidunt vel orci fringilla, viverra sodales eros. Donec cursus metus sit amet dictum finibus. Vestibulum augue ante, scelerisque ut metus non, tincidunt eleifend lacus. Nullam sed ligula nec quam.",
+  },
+  {
+    question: "¿Vivamus pulvinar cursus nulla, malesuada semper sem tincidunt?",
+    answer:
+      "Proin est orci, elementum sit amet enim et, mollis ultricies nibh. Praesent mattis metus non tortor aliquam, eu condimentum justo molestie. Aliquam condimentum ultrices felis, id interdum purus vulputate quis. Nunc sollicitudin turpis non dolor tempor maximus. Vestibulum ante ipsum primis in faucibus orci luctus.",
+  },
+  {
+    question: "¿Aliquam efficitur vitae lorem ut ornare. Proin laoreet?",
+    answer:
+      "Nam tempor eros ut facilisis fermentum. Maecenas bibendum, purus eget ultricies maximus, massa dui consequat sapien, ac laoreet quam nunc at purus. Proin ac varius arcu. Ut sodales semper maximus. Aliquam eget felis quis nulla rhoncus eleifend id eget tellus. Vivamus vestibulum ut orci bibendum.",
+  },
+];
+
+export const TESTIMONIALS = [
+  {
+    client: "Cecilio Manzanares",
+    type: "Comprador",
+    picture: "/images/male-1.jpg",
+    text: "Etiam condimentum mauris viverra elit ullamcorper, in pellentesque lectus faucibus.Nullam sodales, turpis quis luctus euismod, magna sem egestas ipsum, id posuere ex eros sed.",
+  },
+  {
+    client: "Daila Zapata Jiménez",
+    type: "Vendedora",
+    picture: "/images/female-1.jpg",
+    text: "Fusce finibus, orci a imperdiet semper, dui justo vestibulum orci, id ornare arcu tellus ut metus. Integer gravida nibh neque, ac lobortis neque ornare id. Phasellus ac pharetra orci, at tristique nibh. Aliquam ac augue.",
+  },
+  {
+    client: "Francisco Cardona Cadena",
+    type: "Vendedor",
+    picture: "/images/male-2.jpg",
+    text: "Etiam mollis posuere dui, porta dictum neque. Etiam posuere aliquam quam, non facilisis orci euismod quis. Nunc in ullamcorper ipsum. Aenean dapibus libero in maximus.",
+  },
+  {
+    client: "Josefina Morales",
+    type: "Compradora",
+    picture: "/images/female-2.jpg",
+    text: "Duis non orci feugiat, bibendum metus sit amet, dignissim ligula. Donec vel pellentesque quam. Sed vel risus nibh. Cras ipsum tortor, tristique ac magna et, scelerisque aliquet ante. Sed pulvinar diam in enim euismod, vel.",
+  },
+];
