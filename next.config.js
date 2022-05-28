046344d839e30036ea8d28579ca97fcf71dc815a
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-27 22:09:45 -0400

    Eliminada la dep "@svgr/webpack" debido a que ya no es necesaria

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
   images: {
     domains: ["images.prismic.io"],
   },
