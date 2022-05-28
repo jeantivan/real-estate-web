ffc938ea2bc1596cf838e036bc84bddd7bd0edfa
Jean Paul Tivan <jptivan53@gmail.com>
2022-05-27 22:10:19 -0400

    Eliminados los iconos svg

--- src/icons/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FindHouse } from "./find-house.svg";
-export { default as Goals } from "./goals.svg";
-export { default as Market } from "./market.svg";
-export { default as Rent } from "./rent.svg";
-export { default as RealEstate } from "./real-estate.svg";
