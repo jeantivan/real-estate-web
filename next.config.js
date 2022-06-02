2a61a19978cb17a4ca1007a6c0429665d022e41b
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-02 18:24:56 -0400

    Comando para analizar en detalle el Bundle del projecto

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ["images.prismic.io"],
   },
@@ -15,4 +19,4 @@ module.exports = {
     ];
   },
   swcMinify: true,
-};
+});
