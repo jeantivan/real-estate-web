9dc988791eec21918d8813c82367e1239a19b8e3
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-02 17:49:25 -0400

    Cambios en la UI y estilos

--- src/layouts/inmueble/overview.js
+++ src/layouts/inmueble/overview.js
@@ -2,8 +2,17 @@ import React from "react";
 import { Paper, Divider, Typography, Grid, Box } from "@mui/material";
 import { PrismicRichText } from "@prismicio/react";
 
+const sxBase = (theme) => ({
+  pl: 2,
+  borderStyle: "solid",
+  borderWidth: 0,
+  borderColor: theme.palette.primary.main,
+  borderLeftWidth: 3,
+  borderRadius: 1,
+  background: `linear-gradient(to right, ${theme.palette.primary[50]} 0% ,  transparent 10%) `,
+});
+
 export function OverviewSection({
-  id,
   tipo,
   area,
   habitaciones,
@@ -11,85 +20,138 @@ export function OverviewSection({
   estacionamientos,
   antiguedad,
   descripcion,
+  estado,
+  precio,
 }) {
   return (
-    <Grid item xs={12} md={8}>
-      <Paper
-        component="section"
-        id="caracteristicas-del-inmueble"
-        elevation={0}
-        sx={{ p: 2, minHeight: "100%" }}
-      >
+    <Grid
+      item
+      xs={12}
+      md={8}
+      component="section"
+      id="caracteristicas-del-inmueble"
+    >
+      <Paper elevation={0} sx={{ p: 2, minHeight: "100%" }}>
         <Box mb={3}>
-          <Grid
-            container
-            mb={3}
-            justifyContent="space-between"
-            alignItems="baseline"
-          >
-            <Grid item>
-              <Typography component="h3" variant="h6" fontWeight={700}>
-                Características del inmueble.
+          <Typography component="h3" variant="h6" fontWeight={700} mb={1}>
+            Características del inmueble.
+          </Typography>
+          <Grid container mb={3} justifyContent="space-between">
+            <Grid item xs={4} lg={3}>
+              <Typography
+                variant="body2"
+                textTransform="uppercase"
+                mb={1}
+                display="block"
+              >
+                Inmueble en:
+              </Typography>
+              <Typography
+                component="p"
+                variant="h6"
+                sx={(theme) => ({
+                  textAlign: "center",
+                  padding: theme.spacing(0.5, 1),
+                  borderRadius: 2,
+                  fontWeight: 500,
+                  backgroundColor:
+                    estado === "Venta"
+                      ? theme.palette.success.main
+                      : theme.palette.secondary.main,
+                  color:
+                    estado === "Venta"
+                      ? theme.palette.success.contrastText
+                      : theme.palette.secondary.contrastText,
+                })}
+              >
+                {estado}
               </Typography>
             </Grid>
-            <Grid item>
-              <Typography variant="body2" color="textSecondary">
-                <strong>ID: </strong>
-                {id}
+            <Grid item xs={6} sm={4}>
+              <Typography
+                variant="body2"
+                textTransform="uppercase"
+                mb={1}
+                display="block"
+              >
+                Precio:
+              </Typography>
+              <Typography
+                component="p"
+                variant="h4"
+                color="primary.dark"
+                fontWeight={700}
+              >
+                <Typography component="span" variant="h5" fontWeight={700}>
+                  $
+                </Typography>
+                {Intl.NumberFormat("es-VE").format(precio)}
               </Typography>
             </Grid>
           </Grid>
           <Grid container spacing={2}>
-            <Grid item xs lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Tipo
-              </Typography>
-              <Typography component="p" variant="h6">
-                {tipo}
-              </Typography>
+            <Grid item xs={6} sm={4}>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Tipo
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {tipo}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={6}>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Antiguedad
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {antiguedad} años
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={6} sm={4} lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Area
-              </Typography>
-              <Typography component="p" variant="h6">
-                {area + " "}
-                <span>
-                  m<sup>2</sup>
-                </span>
-              </Typography>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Area
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {area + " "}
+                  <span>
+                    m<sup>2</sup>
+                  </span>
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={6} sm={4} lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Habitaciones
-              </Typography>
-              <Typography component="p" variant="h6">
-                {habitaciones}
-              </Typography>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Habitaciones
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {habitaciones}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} sm={4} lg={2}>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Baños
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {banos}
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={6} sm={4} lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Baños
-              </Typography>
-              <Typography component="p" variant="h6">
-                {banos}
-              </Typography>
-            </Grid>
-            <Grid item xs={6} sm={4} lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Estacionamientos
-              </Typography>
-              <Typography component="p" variant="h6">
-                {estacionamientos}
-              </Typography>
-            </Grid>
-            <Grid item xs={6} sm={4} lg={3}>
-              <Typography variant="caption" gutterBottom>
-                Antiguedad
-              </Typography>
-              <Typography component="p" variant="h6">
-                {antiguedad}
-              </Typography>
+              <Box sx={sxBase}>
+                <Typography variant="caption" gutterBottom>
+                  Estacionamientos
+                </Typography>
+                <Typography component="p" variant="h6">
+                  {estacionamientos}
+                </Typography>
+              </Box>
             </Grid>
           </Grid>
         </Box>
