6df0a59114a598ebb6f2ad64591b1ef5bb7cec0b
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-03 21:40:57 -0400

    Sección Header para la pág "home"

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import { Layout } from "@/components";
-import { Container, Typography } from "@mui/material/";
+import { HeaderSection } from "@/layouts/home";
 
 export default function Home() {
   return (
@@ -8,23 +8,7 @@ export default function Home() {
       titulo="Inicio"
       description="Maecenas quis imperdiet ligula. Phasellus convallis urna nunc, sit amet posuere ex gravida at. Nam ac interdum odio, a condimentum orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in elit risus. Nulla lacus diam, condimentum cursus semper sit amet, sagittis vitae magna. In a urna a urna interdum dictum. In lectus sem, feugiat et dolor sed, pulvinar egestas nisi. Maecenas sodales ultrices nisl nec condimentum."
     >
-      <Container>
-        <Typography variant="h1" component="h1">
-          Avilatek Inmobiliaria
-        </Typography>
-        <Typography>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-          repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-          perferendis temporibus id ex, neque rem voluptate quis tempora esse
-          earum alias voluptates.
-        </Typography>
-        <Typography>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-          repellendus blanditiis tenetur facilis nam architecto iure aspernatur
-          perferendis temporibus id ex, neque rem voluptate quis tempora esse
-          earum alias voluptates.
-        </Typography>
-      </Container>
+      <HeaderSection />
     </Layout>
   );
 }
