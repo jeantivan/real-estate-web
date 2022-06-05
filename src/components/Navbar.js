6c501fa07b9be91ce672d057df9e2b0728103184
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-04 23:01:21 -0400

    Navbar cambia de color al hacer scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import {
   IconButton,
   Toolbar,
   Box,
+  useScrollTrigger,
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
 import { useState } from "react";
@@ -14,8 +15,18 @@ import { Logo } from "./Logo";
 import { NavLinks } from "./NavLinks";
 import { RRSS } from "./RRSS";
 
+const appbarBaseSx = {
+  borderWidth: 0,
+  borderStyle: "solid",
+  borderBottomWidth: 1,
+  transition: "all 0.1s ease",
+};
+
 export function Navbar({ currentPage }) {
   const [show, setShow] = useState(false);
+  const scrollTrigger = useScrollTrigger({ threshold: 1 });
+
+  const isInicioPage = currentPage === "Inicio";
 
   const toggleDrawer = () => {
     setShow(!show);
@@ -26,118 +37,159 @@ export function Navbar({ currentPage }) {
   };
 
   return (
-    <>
-      <AppBar
-        color="inherit"
-        elevation={0}
-        position="fixed"
+    <AppBar
+      color="inherit"
+      elevation={0}
+      position="fixed"
+      sx={[
+        appbarBaseSx,
+        {
+          backdropFilter: scrollTrigger && "saturate(180%) blur(20px)",
+          borderColor: !isInicioPage
+            ? "rgba(0,0,0, 0.12)"
+            : scrollTrigger
+            ? "rgba(0,0,0, 0.12)"
+            : "rgba(0,0,0,0)",
+          backgroundColor: !isInicioPage
+            ? "rgba(255,255, 255, 0.9)"
+            : !scrollTrigger
+            ? "transparent"
+            : "rgba(255,255, 255, 0.9)",
+        },
+      ]}
+    >
+      <Toolbar
+        component={Container}
+        maxWidth="lg"
         sx={{
-          borderWidth: 0,
-          borderColor: "rgba(0,0,0, 0.12)",
-          borderStyle: "solid",
-          borderBottomWidth: 1,
-          backgroundColor: "rgba(255,255, 255, 0.8)",
-          backdropFilter: "saturate(180%) blur(20px)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <Toolbar
-          component={Container}
-          maxWidth="lg"
+        <Box
+          sx={{
+            display: { md: "none" },
+          }}
+        >
+          <IconButton
+            edge="start"
+            aria-label="menu"
+            onClick={toggleDrawer}
+            size="large"
+            sx={{
+              transition: "all 0.1s ease",
+              color: !isInicioPage
+                ? "black"
+                : !scrollTrigger
+                ? "white"
+                : "black",
+            }}
+          >
+            <Menu />
+          </IconButton>
+        </Box>
+        <Box
           sx={{
             display: "flex",
+            flex: 1,
             alignItems: "center",
-            justifyContent: "space-between",
+            flexGrow: { md: 0 },
+            position: "relative",
           }}
         >
           <Box
             sx={{
+              width: "100%",
               display: "flex",
-              flex: 1,
-              alignItems: "center",
-              flexGrow: { md: 0 },
+              justifyContent: "center",
+              mx: "auto",
+              ml: { xs: -1.75, sm: 0 },
+              "& .nav-logo": {
+                transition: "all 0.1s ease",
+                color: !isInicioPage
+                  ? "black"
+                  : !scrollTrigger
+                  ? "white"
+                  : "black",
+              },
             }}
+            ref={ref}
           >
-            <Box
-              sx={{
-                display: { md: "none" },
-              }}
+            <Logo inNav />
+          </Box>
+        </Box>
+        <Box
+          sx={(theme) => ({
+            display: { xs: "none", md: "flex" },
+            flex: 1,
+            alignSelf: "stretch",
+            minHeight: "100%",
+            "& .nav-link": {
+              transition: "all 0.1s ease",
+              color: !isInicioPage
+                ? "black"
+                : !scrollTrigger
+                ? "white"
+                : "black",
+              "&:hover": {
+                color: theme.palette.primary.main,
+              },
+            },
+          })}
+        >
+          <NavLinks currentPage={currentPage} />
+        </Box>
+        <Box
+          sx={(theme) => ({
+            display: { xs: "none", sm: "flex" },
+            ml: "auto",
+            mr: -1.5,
+            alignSelf: "center",
+            "& .redes-icon": {
+              transition: "all 0.1s ease",
+              color: !isInicioPage
+                ? "black"
+                : !scrollTrigger
+                ? "white"
+                : "black",
+              "&:hover": {
+                color: theme.palette.primary.main,
+              },
+            },
+          })}
+        >
+          <RRSS />
+        </Box>
+        <Drawer
+          anchor="left"
+          open={show}
+          onClose={toggleDrawer}
+          component="nav"
+          sx={{ display: "flex", width: 260, pt: 1 }}
+        >
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer}
+              size="large"
+              mr={1}
             >
-              <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={toggleDrawer}
-                size="large"
-              >
-                <Menu />
-              </IconButton>
-            </Box>
-            <Box
-              sx={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-                ml: { xs: -3, md: 0 },
-                /* "& .nav-logo": {
-                  color: scrollYProgress.get() === 0 ? "white" : "black",
-                }, */
-              }}
-            >
+              <Menu />
+            </IconButton>
+            <Box mx="auto">
               <Logo inNav />
             </Box>
+          </Toolbar>
+          <Divider />
+          <DrawerLinks handleClose={handleClose} currentPage={currentPage} />
+          <Box mt="auto" sx={{ "& .redes-icon": { color: "black" } }}>
+            <RRSS />
           </Box>
-          <Drawer
-            anchor="left"
-            open={show}
-            onClose={toggleDrawer}
-            component="nav"
-            sx={{ display: "flex", width: 260, pt: 1 }}
-          >
-            <Toolbar>
-              <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                onClick={toggleDrawer}
-                size="large"
-                mr={1}
-              >
-                <Menu />
-              </IconButton>
-              <Box mx="auto">
-                <Logo inNav />
-              </Box>
-            </Toolbar>
-            <Divider />
-            <DrawerLinks handleClose={handleClose} currentPage={currentPage} />
-
-            <Box
-              mt="auto"
-              sx={{
-                ".& > *": {
-                  color: "white",
-                },
-              }}
-            >
-              <RRSS />
-            </Box>
-          </Drawer>
-          <Box
-            sx={{
-              display: { xs: "none", md: "flex" },
-              flex: 1,
-              alignSelf: "stretch",
-              minHeight: "100%",
-            }}
-          >
-            <NavLinks currentPage={currentPage} />
-            <Box ml="auto" mr={-0.75} alignSelf="center">
-              <RRSS />
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {/* {(currentPage !== "Inicio" || !currentPage !== "Nosotros") && <Toolbar />} */}
-    </>
+        </Drawer>
+      </Toolbar>
+    </AppBar>
   );
 }
