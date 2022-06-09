d76fc2086d7f3eed54b5edcad182a880f6350598
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-08 21:39:56 -0400

    Sección de venta para la página Inicio

--- src/layouts/home/index.js
+++ src/layouts/home/index.js
@@ -1,1 +1,4 @@
 export { HeaderSection } from "./header";
+export { ServicesSection } from "./services";
+export { LatestInmuebles } from "./latest";
+export { SalesSection } from "./sales";
---
+++ src/layouts/home/sales.js
@@ -0,0 +1,68 @@
+import { Container, Box, Grid, Typography } from "@mui/material";
+
+export function SalesSection() {
+  return (
+    <Box bgcolor="primary.dark">
+      <Container sx={{ py: 3 }}>
+        <Grid container color="white" justifyContent="center">
+          <Grid
+            item
+            xs={12}
+            sm={3}
+            container
+            justifyContent="center"
+            alignItems="flex-end"
+          >
+            <Box px={3} pt={7}>
+              <Typography variant="h3" fontWeight={700}>
+                100+
+              </Typography>
+              <Typography>
+                De inmuebles vendidos a través de la compañía
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={3}
+            container
+            justifyContent="center"
+            alignItems="flex-end"
+            sx={(theme) => ({
+              borderWidth: 0,
+              borderColor: theme.palette.primary.light,
+              borderStyle: "solid",
+              borderLeftWidth: 1,
+              borderRightWidth: 1,
+            })}
+          >
+            <Box px={3} pt={7}>
+              <Typography variant="h3" fontWeight={700}>
+                50+
+              </Typography>
+              <Typography>
+                De inmuebles publicados en nuestra plataforma
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={3}
+            container
+            justifyContent="center"
+            alignItems="flex-end"
+          >
+            <Box px={3} pt={7}>
+              <Typography variant="h3" fontWeight={700}>
+                500+
+              </Typography>
+              <Typography>Familias felices en con su nuevo hogar</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
